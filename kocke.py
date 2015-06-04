f86e7ede9a84269efd5476454d59ce994dbec922
jan-golob <jangolob.jg@gmail.com>
2015-06-04 09:46:12 +0200

    dodal točkovanje(alfa)

--- kocke.py
+++ kocke.py
@@ -1,6 +1,48 @@
 import tkinter as tk
 import random
 
+def pari3(met2):
+    par = 0
+    for a in range(1,7):
+        if met2.count(a) == 2:
+            par += 1
+    if par == 3:
+        return True
+    else:
+        return False
+
+def oceni(met):
+    tocke = 0
+    kocke = len(met)
+    ##preverimo za mete 6 kock
+    if len(met)== 6:
+        if met == [1,2,3,4,5,6]:
+            return (1500, 0)
+        elif pari3(met):
+            return (1500, 0)
+
+    ##preverimo za mete 3 ali več kock
+    if len(met) >= 3:
+        if met.count(1) >= 3:
+            tocke += 1000*(met.count(1) - 2)
+            kocke = kocke - met.count(1)
+        for a in range(2,7):
+            if met.count(a) >= 3:
+                tocke += a*100*(met.count(a) - 2)
+                kocke = kocke - (met.count(a))
+                ##prevermo še za ostalo
+
+    for a in [1,5]:
+        if met.count(a) < 3:
+            if a == 1:
+                x = 100
+            else:
+                x = 50
+            tocke += met.count(a) * x
+            kocke = kocke - met.count(a)
+    return (tocke, kocke)
+
+
 ##def roll(k=1):
 ##    meti = []
 ##    for a in range(k):
@@ -50,9 +92,15 @@ class tabela:
 ##        dice1.frame.grid(row = 0, column = 0)
 
     def spusti(self):
+        pike = []
         for a in self.kocke:
             if a.cup.get():
                 a.roll()
+                pike.append(a.plat.get())
+        a=oceni(pike)
+        print (a)
+
+    
 
 
 root=tk.Tk()
